c2cbf97eb415d436d3bf2f1d41afca44a3fb3310
= <=>
2023-03-14 16:39:54 +0100

    new file:   .dart_tool/dartpad/web_plugin_registrant.dart 	new file:   .dart_tool/package_config.json 	new file:   .dart_tool/package_config_subset 	new file:   .dart_tool/version 	new file:   .metadata 	new file:   analysis_options.yaml 	new file:   android/.gitignore 	new file:   android/app/build.gradle 	new file:   android/app/src/debug/AndroidManifest.xml 	new file:   android/app/src/main/AndroidManifest.xml 	new file:   android/app/src/main/kotlin/com/example/hangman_game_tut/MainActivity.kt 	new file:   android/app/src/main/res/drawable-v21/launch_background.xml 	new file:   android/app/src/main/res/drawable/launch_background.xml 	new file:   android/app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   android/app/src/main/res/values-night/styles.xml 	new file:   android/app/src/main/res/values/styles.xml 	new file:   android/app/src/profile/AndroidManifest.xml 	new file:   android/build.gradle 	new file:   android/gradle.properties 	new file:   android/gradle/wrapper/gradle-wrapper.properties 	new file:   android/settings.gradle 	new file:   assets/body.png 	new file:   assets/hang.png 	new file:   assets/head.png 	new file:   assets/la.png 	new file:   assets/ll.png 	new file:   assets/ra.png 	new file:   assets/rl.png 	new file:   lib/main.dart 	new file:   lib/ui/colors.dart 	new file:   lib/ui/widget/figure_image.dart 	new file:   lib/ui/widget/letter.dart 	new file:   lib/utils/game.dart 	new file:   pubspec.lock 	new file:   pubspec.yaml 	new file:   test/widget_test.dart 	new file:   web/favicon.png 	new file:   web/icons/Icon-192.png 	new file:   web/icons/Icon-512.png 	new file:   web/icons/Icon-maskable-192.png 	new file:   web/icons/Icon-maskable-512.png 	new file:   web/index.html 	new file:   web/manifest.json

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'ui/widget/figure_image.dart';
+import 'ui/widget/letter.dart';
+import 'utils/game.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({Key? key}) : super(key: key);
+
+  @override
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  String word = "Hello".toUpperCase();
+  List<String> alphabets = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 14, 9, 85),
+      appBar: AppBar(
+        title: Text("Hangman"),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 14, 9, 85),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Stack(
+              children: [
+                figureImage(Game.tries >= 0, "assets/hang.png"),
+                figureImage(Game.tries >= 1, "assets/head.png"),
+                figureImage(Game.tries >= 2, "assets/body.png"),
+                figureImage(Game.tries >= 3, "assets/rightarm.png"),
+                figureImage(Game.tries >= 4, "assets/leftarm.png"),
+                figureImage(Game.tries >= 5, "assets/rightleg.png"),
+                figureImage(Game.tries >= 6, "assets/leftleg.png"),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: word
+                .split('')
+                .map((e) => letter(e.toUpperCase(),
+                    !Game.selectedChar.contains(e.toUpperCase())))
+                .toList(),
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 250.0,
+            child: GridView.count(
+              crossAxisCount: 7,
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+              padding: EdgeInsets.all(8.0),
+              children: alphabets.map((e) {
+                return RawMaterialButton(
+                  onPressed: Game.selectedChar.contains(e)
+                      ? null
+                      : () {
+                          setState(() {
+                            Game.selectedChar.add(e);
+                            print(Game.selectedChar);
+                            if (!word.split('').contains(e.toUpperCase())) {
+                              Game.tries++;
+                            }
+                          });
+                        },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: Text(
+                    e,
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  fillColor: Game.selectedChar.contains(e)
+                      ? Color.fromARGB(221, 255, 0, 0)
+                      : Color.fromARGB(255, 255, 255, 7),
+                );
+              }).toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
